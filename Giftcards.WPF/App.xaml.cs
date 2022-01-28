8c0faf76c6c75d65980a64d85b45a7fa85a98f34
Laura Gagliani <laura.gagliani@avanade.com>
2022-01-28 11:19:17 +0100

    .

--- Giftcards.WPF/App.xaml.cs
+++ Giftcards.WPF/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Giftcards.Core.Mock.InMemoryStorage;
+using Giftcards.WPF.ViewModels;
+using Giftcards.WPF.Views;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,17 @@ namespace Giftcards.WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Storage.InitializeMemory();
+
+            GiftcardSummaryView view = new GiftcardSummaryView();
+            GiftcardSummaryViewModel viewmodel = new GiftcardSummaryViewModel();
+            view.DataContext = viewmodel;
+
+            view.Show();
+        }
     }
 }
---
+++ Giftcards.WPF/ViewModels/GiftcardSummaryViewModel.cs
@@ -0,0 +1,61 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Giftcards.Core.BusinessLayers;
+using Giftcards.Core.Entities;
+using Giftcards.Core.Mock.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Giftcards.WPF.ViewModels
+{
+    public class GiftcardSummaryViewModel : ViewModelBase
+    {
+        public ICommand ShowCreateFormCommand { get; set; }
+        public ICommand LoadGiftcardsCommand { get; set; }
+
+        public GiftcardSummaryViewModel()
+        {
+            ShowCreateFormCommand = new RelayCommand(() => ExecuteShowCreateForm());
+            LoadGiftcardsCommand = new RelayCommand(() => ExecuteLoadGiftcards());
+
+            _giftcardSource = new ObservableCollection<GiftcardRowViewModel>();
+            _giftcards = new CollectionView(_giftcardSource);
+
+            LoadGiftcardsCommand.Execute(null);
+        }
+
+        public ObservableCollection<GiftcardRowViewModel> _giftcardSource;
+        private ICollectionView _giftcards;
+        public ICollectionView Giftcards
+        {
+            get { return _giftcards; }
+            set { _giftcards = value; RaisePropertyChanged(); }
+        }
+
+        private void ExecuteLoadGiftcards()
+        {
+            MainBusinessLayer bl = new MainBusinessLayer(new MockGiftcardRepository());
+            IList<Giftcard> giftcards = bl.FetchAllGiftcards();
+
+            _giftcardSource.Clear();
+
+            foreach (var item in giftcards)
+            {
+                GiftcardRowViewModel row = new GiftcardRowViewModel();
+                _giftcardSource.Add(row);
+            }
+        }
+
+        private void ExecuteShowCreateForm()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
